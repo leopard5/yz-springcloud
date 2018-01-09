4b67fc94d1a0b1386296503530b879393be59b28
qiyazhong <qiyazhong@163.com>
2018-01-09 09:00:23 +0800

    eureka client and remove dead code

--- springboot-test/src/main/java/com/yz/test/Application.java
+++ springboot-test/src/main/java/com/yz/test/Application.java
@@ -2,11 +2,13 @@ package com.yz.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- springboot-test/src/main/java/com/yz/test/controller/UserController.java
+++ springboot-test/src/main/java/com/yz/test/controller/UserController.java
@@ -1,28 +1,16 @@
 package com.yz.test.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.sql.DataSource;
-
 @RestController
-//@ConfigurationProperties(prefix = "test")
-/**
- *
- */
 public class UserController {
-
-//    @Autowired
-//    DataSource dataSource;
 
     @RequestMapping("/user")
     public String hello() {
         return "aaaaa";
     }
 
-//    @RequestMapping("/dbpool")
-//    public String test() {
-//        return dataSource.getClass().toString();
-//    }
 }
+
+
