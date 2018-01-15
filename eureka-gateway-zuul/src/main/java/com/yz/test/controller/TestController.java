08dc4a1984ecb65282cdf1edcb4efd4f229409b4
qiyazhong <qiyazhong@163.com>
2018-01-15 17:45:01 +0800

    zuul test

--- eureka-gateway-zuul/src/main/java/com/yz/test/controller/TestController.java
+++ eureka-gateway-zuul/src/main/java/com/yz/test/controller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class HelloController {
+public class TestController {
 
     @RequestMapping("/local/hello")
     public String hello() {
