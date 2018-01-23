feef70ef62e95052e5ee6d3c74615390fb275fb0
qiyazhong <qiyazhong@163.com>
2018-01-23 09:52:09 +0800

    config-client-bus

--- config-client-bus/src/main/java/com/yz/test/controller/TestController.java
+++ config-client-bus/src/main/java/com/yz/test/controller/TestController.java
@@ -3,11 +3,15 @@ package com.yz.test.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
+//@Configuration
 public class TestController {
     private final Logger logger = LoggerFactory.getLogger(TestController.class);
 
