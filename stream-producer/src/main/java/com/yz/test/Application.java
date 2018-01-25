f15220ca108e71e974e9d0d8a3195006c3f479b6
qiyazhong <qiyazhong@163.com>
2018-01-25 09:17:58 +0800

    spring cloud stream

---
+++ stream-producer/src/main/java/com/yz/test/Application.java
@@ -0,0 +1,14 @@
+package com.yz.test;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
