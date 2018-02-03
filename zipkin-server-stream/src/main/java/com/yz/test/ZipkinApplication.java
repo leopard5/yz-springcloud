f0e9cc713026ae1d2c176f4a7ea4bc10d02cbb76
qiyazhong <qiyazhong@163.com>
2018-02-03 08:48:00 +0800

    zipkin-server-stream

--- zipkin-server-stream/src/main/java/com/yz/test/ZipkinApplication.java
+++ zipkin-server-stream/src/main/java/com/yz/test/ZipkinApplication.java
@@ -2,9 +2,9 @@ package com.yz.test;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import zipkin.server.EnableZipkinServer;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
-@EnableZipkinServer
+@EnableZipkinStreamServer
 @SpringBootApplication
 public class ZipkinApplication {
 
